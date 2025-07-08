bf816acc29fc1a86f317fe3f4fa51efd90d43d9c
Che0807 <elpin0428@naver.com>
2025-07-08 18:09:13 +0900

    Initial commit

---
+++ src/main/java/com/example/bookify/User.java
@@ -0,0 +1,11 @@
+package com.example.bookify;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
