7ab9fd19222d2f4634b1499a51d3785b93cf7f3d
Hyeon woo <renzpark@naver.com>
2025-07-09 15:04:57 +0900

    chore(#9): @Builder 삭제, 소프트 딜리트 엔티티 상속 완료

--- src/main/java/com/example/bookify/domain/book/entity/Book.java
+++ src/main/java/com/example/bookify/domain/book/entity/Book.java
@@ -1,18 +1,15 @@
 package com.example.bookify.domain.book.entity;
 
+import com.example.bookify.global.common.jpa.SoftDeleteEntity;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "books")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Builder
-public class Book {
+public class Book extends SoftDeleteEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,35 +24,12 @@ public class Book {
     @Column(length = 100)
     private String publisher;
 
-    private LocalDate publishedDate;
-
     @Column(length = 50)
     private String genre;
 
     @Column(nullable = false)
     private Integer stock = 0;
 
-    @Column(nullable = false)
-    private Boolean isDeleted = false;
-
-    private LocalDateTime deletedAt;
-
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
-
-    private LocalDateTime updatedAt;
-
     @Column
     private Integer ranking;
-
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 }
