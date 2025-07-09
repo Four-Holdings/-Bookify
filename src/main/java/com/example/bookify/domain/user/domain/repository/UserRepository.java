89907868c26c3bfd44807df126e9515fd555d676
gsemily <gsemily22@gmail.com>
2025-07-09 15:35:52 +0900

    hotfix: userRepository 메서드명 수정, User.java 엔티티 삭제
    - existByEmail -> existsByEmail
    - existByNickname -> existsByNickname

--- src/main/java/com/example/bookify/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/bookify/domain/user/domain/repository/UserRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    boolean existByEmail(String email);
-    boolean existByNickname(String nickname);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
