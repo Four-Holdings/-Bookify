ab68de83306c4cafc761cbcbeb268e24a3134ba9
gsemily <gsemily22@gmail.com>
2025-07-09 12:22:42 +0900

    feat: User Entity, UserRole, UserRepository 추가

---
+++ src/main/java/com/example/bookify/domain/user/domain/model/User.java
@@ -0,0 +1,34 @@
+package com.example.bookify.domain.user.domain.model;
+
+import com.example.bookify.global.common.jpa.SoftDeleteEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends SoftDeleteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+}
