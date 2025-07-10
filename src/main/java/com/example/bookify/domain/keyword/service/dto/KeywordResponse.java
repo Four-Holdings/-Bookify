c0318a4c5933889d3ae29205fb3cd3b967df1815
Che0807 <elpin0428@naver.com>
2025-07-10 22:24:16 +0900

    feat: KeywordResponse Validation 추가 및 필드 명 변경

--- src/main/java/com/example/bookify/domain/keyword/service/dto/KeywordResponse.java
+++ src/main/java/com/example/bookify/domain/keyword/service/dto/KeywordResponse.java
@@ -1,13 +1,18 @@
 package com.example.bookify.domain.keyword.service.dto;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@JsonPropertyOrder({"word", "count"})
+@JsonPropertyOrder({"keyword", "count"})
 public class KeywordResponse {
-    private String word;
+
+    @NotBlank(message = "키워드는 빈 값일 수 없습니다.")
+    private String keyword;
+    @PositiveOrZero(message = "카운트는 0 이상이어야 합니다.")
     private long count;
 }
