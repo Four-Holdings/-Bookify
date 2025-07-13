cb588cdeb0bcd6613502f6e112e66a6fca42b658
Che0807 <elpin0428@naver.com>
2025-07-13 18:58:05 +0900

    feat: KeywordRepository에 다중 키워드 조회 메서드 추가
    - List<String> 키워드 목록으로 Keyword 엔티티 조회 기능 구현

--- src/main/java/com/example/bookify/domain/keyword/domain/repository/KeywordRepository.java
+++ src/main/java/com/example/bookify/domain/keyword/domain/repository/KeywordRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface KeywordRepository extends JpaRepository<Keyword,Long> {
@@ -15,4 +17,7 @@ public interface KeywordRepository extends JpaRepository<Keyword,Long> {
     //인기 검색어(Keyword)를 count 기준 내림차순으로 정렬해서 상위 N개만 가져오는 메서드
     @Query("SELECT k FROM Keyword k ORDER BY k.count DESC")
     Page<Keyword> findTopKeywords(Pageable pageable);
+
+    List<Keyword> findByKeywordIn(List<String> keywords);
 }
+
